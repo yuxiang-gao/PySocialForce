4f9ead9fd226344ca28f23853a62dac2948d5162
Yuxiang Gao <yuxiang.gao@jhu.edu>
2020-05-27 12:16:16 -0400

    init dev env

--- setup.py
+++ setup.py
@@ -1,48 +1,36 @@
 from setuptools import setup
 
 # extract version from __init__.py
-with open('socialforce/__init__.py', 'r') as f:
-    VERSION_LINE = [l for l in f if l.startswith('__version__')][0]
-    VERSION = VERSION_LINE.split('=')[1].strip()[1:-1]
+with open("socialforce/__init__.py", "r") as f:
+    VERSION_LINE = [l for l in f if l.startswith("__version__")][0]
+    VERSION = VERSION_LINE.split("=")[1].strip()[1:-1]
 
 
 setup(
-    name='socialforce',
+    name="socialforce",
     version=VERSION,
-    packages=[
-        'socialforce',
-    ],
-    license='MIT',
-    description='Numpy implementation of the Social Force model.',
-    long_description=open('README.rst').read(),
-    author='Sven Kreiss',
-    author_email='me@svenkreiss.com',
-    url='https://github.com/svenkreiss/socialforce',
-
-    install_requires=[
-        'numpy',
-    ],
+    packages=["socialforce",],
+    license="MIT",
+    description="Numpy implementation of the Social Force model.",
+    long_description=open("README.rst").read(),
+    author="Sven Kreiss",
+    author_email="me@svenkreiss.com",
+    url="https://github.com/svenkreiss/socialforce",
+    install_requires=["numpy",],
     extras_require={
-        'test': [
-            'pylint',
-            'pytest',
-        ],
-        'plot': [
-            'matplotlib',
-        ],
+        "dev": ["black", "jupyter"],
+        "test": ["pylint", "pytest",],
+        "plot": ["matplotlib",],
     },
-
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
 )
