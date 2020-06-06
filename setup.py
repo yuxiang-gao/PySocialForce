ddc4b3acbb799772b8039dad3608a409f4016116
Yuxiang Gao <yuxiang.gao@jhu.edu>
2020-06-06 01:20:49 -0400

    :construction_worker: Config Travis CI

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author="Sven Kreiss, Yuxiang Gao",
     author_email="me@svenkreiss.com, yuxiang.gao@jhu.edu",
     url="https://github.com/yuxiang-gao/socialforce",
-    install_requires=["numpy", "toml", "numba"],
+    install_requires=["numpy", "scipy", "toml", "numba"],
     extras_require={
         "dev": ["black", "jupyter"],
         "test": ["pylint", "pytest",],
@@ -36,7 +36,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
