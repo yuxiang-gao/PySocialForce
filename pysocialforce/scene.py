2e29d928fa98299dea1c9560d5a6811589b85adb
evnor <53000234+evnor@users.noreply.github.com>
2021-11-30 13:44:07 +0100

    Update scene.py

--- pysocialforce/scene.py
+++ pysocialforce/scene.py
@@ -1,4 +1,4 @@
-"""This module tracks the state odf scene and scen elements like pedestrians, groups and obstacles"""
+"""This module tracks the state of scene and scene elements like pedestrians, groups and obstacles"""
 from typing import List
 
 import numpy as np
@@ -7,9 +7,9 @@ from pysocialforce.utils import stateutils
 
 
 class PedState:
-    """Tracks the state of pedstrains and social groups"""
+    """Tracks the state of pedestrians and social groups"""
 
-    def __init__(self, state, groups, config):
+    def __init__(self, state, goals, groups, config):
         self.default_tau = config("tau", 0.5)
         self.step_width = config("step_width", 0.4)
         self.agent_radius = config("agent_radius", 0.35)
@@ -20,6 +20,7 @@ class PedState:
 
         self.ped_states = []
         self.group_states = []
+        self.goals = goals
 
         self.update(state, groups)
 
@@ -57,6 +58,10 @@ class PedState:
 
     def goal(self) -> np.ndarray:
         return self.state[:, 4:6]
+    
+    def next_goal(self) -> np.ndarray:
+        return self.goals[:, :2]
+
 
     def tau(self):
         return self.state[:, 6:7]
